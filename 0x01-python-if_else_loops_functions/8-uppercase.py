30a61ff424d59760cce4653eb64a87798f1ca839
VanessJames <vanessjames10@gmail.com>
2023-08-08 19:45:35 -0700

    7, 8, 9

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        if ord('a') <= ord(char) <= ord('z'):
+            diff = ord('A') - ord('a')
+            uppercase_char = chr(ord(char) + diff)
+            print("{}".format(uppercase_char), end="")
+        else:
+            print("{}".format(char), end="")
+    print("")
+
+
+uppercase("Hello, World!")
