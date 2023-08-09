02aca0e8b82be604afcd1f2cb99afec7ac3e54a5
VanessJames <vanessjames10@gmail.com>
2023-08-08 19:18:41 -0700

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        con = ", " if tens_digit < 8 else ""
+        con = ", " if tens_digit < 8 or (tens_digit == 8 and ones_digit == 9) else ""
         print("{:d}{:d}".format(tens_digit, ones_digit), end=con)
