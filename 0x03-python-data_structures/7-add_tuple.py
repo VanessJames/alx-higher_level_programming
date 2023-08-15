eb68ecd22197a70fd815e8fcca4b36bace328b50
VanessJames <vanessjames10@gmail.com>
2023-08-14 17:57:14 -0700

    0-12

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
+
+# Example usage
+tuple1 = (1, 2)
+tuple2 = (3, 4)
+result_tuple = add_tuple(tuple1, tuple2)
+print(result_tuple)
